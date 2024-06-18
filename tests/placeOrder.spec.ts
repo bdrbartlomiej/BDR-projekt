9ce0b7f8c39cfeb3c10247ae56531bde106925e3
Bartlomiej <bartlomiejaz@gmail.com>
2024-06-18 08:40:48 +0200

    Projekt BDR v 1.0.0

--- tests/placeOrder.spec.ts
+++ tests/placeOrder.spec.ts
@@ -55,6 +55,7 @@ async function selectPaymentType(page: Page) {
 }
 
 async function buyAndPay(page: Page) {
+  await page.waitForTimeout(2000);
   await page.getByRole('button', { name: 'Kupuję i płacę' }).click();
   await expect(page.getByText('Dziękujemy. Otrzymaliśmy Twoje zamówienie.')).toBeVisible();
 }
